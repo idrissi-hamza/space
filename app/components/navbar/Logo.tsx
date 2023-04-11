88660e836d6d6568ade0268d6387230d45202502
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-04-11 12:13:50 +0000

    fix logo style

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div className='flex place-items-center gap-2 font-bold tracking-widest'>
+    <div className='hidden md:flex md:place-items-center gap-2 font-bold tracking-widest cursor-pointer'>
     <Image 
     alt='logo'
-    className='hidden md:block cursor-pointer'
+    className=''
     height={50}
     width={50}
     src='/images/logo.png'

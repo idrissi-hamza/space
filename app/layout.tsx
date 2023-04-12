971378a584630c7f020bc9a3a178e2a7e5978999
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-04-12 01:18:47 +0000

    fix hydration issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/Navbar';
 import './globals.css';
 import { Nunito } from 'next/font/google';
 import ToasterProvider from './providers/ToasterProvider';
+import ClientOnly from './components/ClientOnly';
 
 export const metadata = {
   title: 'Space',
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <ToasterProvider />
-        <Navbar />
-        <RegisterModal />
+        <ClientOnly>
+          <ToasterProvider />
+          <Navbar />
+          <RegisterModal />
+        </ClientOnly>
         {children}
       </body>
     </html>

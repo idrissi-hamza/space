a883653a194036bd4eddeafb9f777fe0e0887e83
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-07-25 14:01:52 +0100

    fix style

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,15 +7,17 @@ import UserMenu from './UserMenu';
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full bg-white  z-10 shadow-sm">
-      <div className="py-3 border-b-[1px]">
-        <Container>
-          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-            <Logo />
-            <Search />
-            <UserMenu />
-          </div>
-        </Container>
+    <div className='pb-20'>
+      <div className="fixed w-full bg-white  z-10 shadow-sm ">
+        <div className="py-3 border-b-[1px]">
+          <Container>
+            <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+              <Logo />
+              <Search />
+              <UserMenu />
+            </div>
+          </Container>
+        </div>
       </div>
     </div>
   );

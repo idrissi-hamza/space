890afc1b22907383de7f78e4a1110d57d9e47ef6
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-08-01 12:05:43 +0100

    fix logo to direct to main page

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,20 +1,24 @@
-'use client'
-import Image from 'next/image'
-import React from 'react'
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const Logo = () => {
   return (
-    <div className='hidden md:flex md:place-items-center gap-2 font-bold tracking-widest cursor-pointer'>
-    <Image 
-    alt='logo'
-    className=''
-    height={40}
-    width={40}
-    src='/images/logo.png'
-    />
-    SPACE
-    </div>
-  )
-}
+    <Link
+      href="/"
+      className="hidden md:flex md:place-items-center gap-2 font-bold tracking-widest cursor-pointer"
+    >
+      <Image
+        alt="logo"
+        className=""
+        height={40}
+        width={40}
+        src="/images/logo.png"
+      />
+      SPACE
+    </Link>
+  );
+};
 
-export default Logo
+export default Logo;

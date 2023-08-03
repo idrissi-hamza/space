20f9eb7af0b2247002de80adb02089fc25583364
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-08-03 13:14:04 +0100

    fix reservationView messages

--- app/reservations/ReservationsView.tsx
+++ app/reservations/ReservationsView.tsx
@@ -47,8 +47,8 @@ const ReservationsView = ({
   return (
     <Container>
       <Heading
-        title="Trips"
-        subtitle="Where you've been and where you're going"
+        title="Reservations"
+        subtitle="Bookings on your properties"
       />
       <div className=" mt-10 grid  grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-5 2xl:grid-cols-6  gap-8 ">
         {reservations.map((reservation: any) => (
